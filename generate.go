bd9cb360d5b0ab1dd75745b85b5b6dc0150010f1
Thomas De Meyer <t.demeyer@labdigital.nl>
2023-08-11 12:14:23 +0200

    feat: added changie and test pipelines

--- generate.go
+++ generate.go
@@ -1,7 +1,3 @@
 package main
 
-import (
-	_ "github.com/deepmap/oapi-codegen/cmd/oapi-codegen"
-)
-
 //go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config oapi-config.yaml ./openapi.yaml
